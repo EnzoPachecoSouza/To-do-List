8c8f70b030cfcf0d1a32dab02deff3c61ede07af
Enzo Pacheco <enzo.pach@gmail.com>
2025-01-01 22:45:25 -0300

    removendo individualmente

--- index.js
+++ index.js
@@ -4,12 +4,28 @@ let tasks = [
     {id: 3, description: "Escovar os Dentes", checked: false},
 ]
 
+const removeTask = (taskId) => {
+    tasks = tasks.filter(({id}) => parseInt(id) !== parseInt(taskId));
+
+    document
+        .getElementById("to-do-list")
+        .removeChild(document.getElementById(taskId));
+}
+
 const createTaskListItem = (task, checkbox) => {
     const list = document.getElementById("to-do-list");
     const toDo = document.createElement("li");
 
+    const removeTaskButton = document.createElement("button");
+    removeTaskButton.textContent = "X";
+    removeTaskButton.ariaLabel = "Remover tarefa";
+
+    removeTaskButton.onclick = () => removeTask(task.id);
+
     toDo.id = task.id;
     toDo.appendChild(checkbox);
+
+    toDo.appendChild(removeTaskButton);
 
     list.appendChild(toDo);
 
@@ -66,22 +82,32 @@ const createTask = (event) => {
 }
 
 //window.onload - O que está dentro da função atribuída ao window.onload será executado após o carregamento completo da página.
+// window.onload = function() {
+//     const form = document.getElementById("create-to-do-form");
+//     form.addEventListener("submit", createTask);
+    
+//     tasks.forEach(function(task) { //O método forEach é usado para percorrer cada item no array tasks. Para cada tarefa no array, ele executa a função que está dentro do forEach
+//         const checkbox = getCheckBoxInput(task);
+
+//         const list = document.getElementById("to-do-list");//A variável list pega o elemento HTML com o id igual a "to-do-list", usando document.getElementById.
+//         const toDo = document.createElement("li");//A variável toDo cria um novo elemento <li> (lista) para ser adicionado à lista.
+//         //const button = document.createElement("button");//A variável button cria um novo botão para ser adicionado ao <li> criado acima.
+
+//         toDo.id = task.id;
+//         toDo.appendChild(checkbox);
+//         //toDo.appendChild(button)
+
+//         list.appendChild(toDo);//O método appendChild adiciona o elemento <li> criado (toDo) como um filho do elemento list (a <ul> com id="to-do-list").
+//         //Esse passo insere o <li> dentro do <ul>
+//     })
+// }
+
 window.onload = function() {
     const form = document.getElementById("create-to-do-form");
     form.addEventListener("submit", createTask);
-    
-    tasks.forEach(function(task) { //O método forEach é usado para percorrer cada item no array tasks. Para cada tarefa no array, ele executa a função que está dentro do forEach
+
+    tasks.forEach(function(task) {
         const checkbox = getCheckBoxInput(task);
-
-        const list = document.getElementById("to-do-list");//A variável list pega o elemento HTML com o id igual a "to-do-list", usando document.getElementById.
-        const toDo = document.createElement("li");//A variável toDo cria um novo elemento <li> (lista) para ser adicionado à lista.
-        //const button = document.createElement("button");//A variável button cria um novo botão para ser adicionado ao <li> criado acima.
-
-        toDo.id = task.id;
-        toDo.appendChild(checkbox);
-        //toDo.appendChild(button)
-
-        list.appendChild(toDo);//O método appendChild adiciona o elemento <li> criado (toDo) como um filho do elemento list (a <ul> com id="to-do-list").
-        //Esse passo insere o <li> dentro do <ul>
-    })
-}
+        createTaskListItem(task, checkbox); // Reutiliza a função que já adiciona o botão de remover.
+    });
+};
